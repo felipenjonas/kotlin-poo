611eedcac8d8e8dd35c148d731f86546b6f8dfea
Felipe Jonas <felipenjonas@gmail.com>
2021-07-10 10:55:40 -0300

    rename package name

---
+++ src/main/kotlin/teste/digitalBank/Conta.kt
@@ -0,0 +1,16 @@
+package teste.digitalBank
+
+import java.math.BigDecimal
+
+class Conta (
+    val agencia: String,
+    val conta: String,
+    val saldo: BigDecimal
+    ) {
+        fun deposito(valor:BigDecimal){
+
+        }
+        fun saque(valor: BigDecimal){
+
+        }
+}
