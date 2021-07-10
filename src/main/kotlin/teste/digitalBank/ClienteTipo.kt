0b8674e152910e7f305924b019ab19d0d2ec19e9
Felipe Jonas <felipenjonas@gmail.com>
2021-07-10 12:38:42 -0300

    create enum class

---
+++ src/main/kotlin/teste/digitalBank/ClienteTipo.kt
@@ -0,0 +1,6 @@
+package teste.digitalBank
+
+enum class ClienteTipo(val descricao: String) {
+    PF("Pessoa Física"),
+    PJ("Pessoa Jurídica");
+}
---
+++ src/main/kotlin/teste/digitalBank/tests/cliente-tipo-test.kt
@@ -0,0 +1,19 @@
+package teste.digitalBank.tests
+
+import teste.digitalBank.ClienteTipo
+
+fun main(){
+    ClienteTipo.values().forEach { elemento ->
+        println("${elemento.name} - ${elemento.descricao}")
+    }
+
+    ClienteTipo.values().forEach {
+        println("${it.name} - ${it.descricao}")
+    }
+
+    val pf = ClienteTipo.PF
+    println(">> ${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println(">> ${pj.name} - ${pj.descricao}")
+}
