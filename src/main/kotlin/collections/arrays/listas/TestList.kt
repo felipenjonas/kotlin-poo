b5b687a77796bf8de5b2d9734bbfce93105b4306
Felipe Jonas <felipenjonas@gmail.com>
2021-07-17 12:19:06 -0300

    feat: add GroupBy

--- src/main/kotlin/collections/arrays/listas/TestList.kt
+++ src/main/kotlin/collections/arrays/listas/TestList.kt
@@ -1,21 +1,37 @@
 package collections.arrays.listas
 
 fun main() {
-    val joao = Funcionario("João", 1000.0)
-    val maria = Funcionario("Maria", 4000.0)
-    val carlos = Funcionario("Carlos", 2000.0)
+    val joao = Funcionario("João", 1000.0,"Tecnologia" )
+    val maria = Funcionario("Maria", 4000.0, "Recursos Humanos")
+    val carlos = Funcionario("Carlos", 2000.0, "Contabilidade")
+    val andre = Funcionario("André", 4000.0, "Contabilidade")
 
-    val colaboradores = listOf(joao,maria, carlos)
+    val colaboradores = listOf(joao,maria, carlos,andre)
 
 //    Mostra cada um do funcionários em suas posições no Array
     colaboradores.forEach { println(it)}
 //    Procura e printa Somente igual Carlos
     println(colaboradores.find { it.nome == "Maria" })
+//Usando o sortedBy encadeado
+    println("\n------- Usando o sortedBy salario: ")
+    colaboradores
+        .sortedBy { it.salario }
+        .forEach { println(it) }
+// Usando o GroupBy por setor
+    println("\n------- Usando o groupBy por setor: ")
+    colaboradores
+        .groupBy { it.setor }
+        .forEach { println(it) }
+
+
 }
+
+
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val setor: String
 ){
     override fun toString(): String =
         """
