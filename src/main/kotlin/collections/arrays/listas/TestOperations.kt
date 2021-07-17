3d4504b04fa4a396228a3ae65dce1de993ef5cbf
Felipe Jonas <felipenjonas@gmail.com>
2021-07-17 10:24:35 -0300

    feat: Add file Operations Tests

---
+++ src/main/kotlin/collections/arrays/listas/TestOperations.kt
@@ -0,0 +1,29 @@
+package collections.arrays.listas
+
+fun main() {
+    val salarios = doubleArrayOf(1000.0,2000.0,4000.0)
+
+//    Iterar
+    for(salario in salarios){
+        println(salario)
+    }
+    println("--------------------------------")
+    println("Maior Salário: ${salarios.max()}")
+    println("Menor Salário: ${salarios.min()}")
+    println("Média Salarial: ${salarios.average()}")
+
+    println("Salários maiores que R$2500:")
+    val salariosFilter = salarios.filter { it > 2500.0 }
+    salariosFilter.forEach { println(it) }
+
+    print("Quantos salários estão entre 2k e 4k:")
+    println(salarios.count { it in 2000.0..4000.0 })
+
+    println("Procurando salários específicos: ")
+    println(salarios.find {it == 4000.0})
+    println(salarios.find {it == 10000.0})
+
+    println("Procurando qualquer salário válido: ")
+    println(salarios.any {it == 4000.0})
+    println(salarios.any {it == 10000.0})
+}
